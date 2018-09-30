df7d6a3400f5a19e5014369b50c6faf6cbfbff57
vijchint <chintakindivijay84@gmail.com>
2018-10-01 01:15:34 +0530

    my first push

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Created by DELL on 7/19/2018.
+ */
+public class Hello {
+    public static void main(String[] args){
+       /* System.out.println("Hello World");
+        int number =5;
+        System.out.println(number);*/
+    }
+
+    public static void checkPrime(int n){
+        if(n==1){
+            System.out.println();
+        }
+    }
+}
